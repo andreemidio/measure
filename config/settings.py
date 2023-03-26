dcb3ce1b36ea06e14c2f71b352ef1a8db1946763
Andre Emidio <andresjc2008@gmail.com>
2023-03-26 12:55:33 -0300

    restruturação do projeto

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,10 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static/', ]
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
+WHITENOISE_MANIFEST_STRICT = False
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
