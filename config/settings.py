f7858e4d71bd02102ec9321f1a64e9b3c564891a
Andre Emidio <andresjc2008@gmail.com>
2023-03-26 14:34:09 -0300

    restruturação do projeto

--- config/settings.py
+++ config/settings.py
@@ -185,7 +185,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 # STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
-# STATICFILES_DIRS = [BASE_DIR / 'staticfiles', ]
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
