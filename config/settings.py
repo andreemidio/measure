f914fbee9211182559f7293386bea6f95fa3d8be
Andre Emidio <andresjc2008@gmail.com>
2023-03-26 21:01:20 -0300

    restruturação do projeto

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ APPEND_SLASH = False
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
