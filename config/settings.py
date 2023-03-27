d5c2336e7968f2719a48819998f088253cafc21a
Andre Emidio <andresjc2008@gmail.com>
2023-03-27 09:48:35 -0300

    restruturação do projeto

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,6 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
         # 'rest_framework.permissions.AllowAny',
