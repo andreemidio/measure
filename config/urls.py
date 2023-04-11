d2d397fd598de03e1af94b411e83ee70c9c72c59
Basilio40 <42722896+Basilio40@users.noreply.github.com>
2023-04-11 05:08:04 -0300

    ajuste no visual

--- config/urls.py
+++ config/urls.py
@@ -50,6 +50,4 @@ urlpatterns = [
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^docs/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
